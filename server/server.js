b9a4e30859cfb8bb8d81aed7903c4f35d66fd254
Manavp2305 <145106413+Manavp2305@users.noreply.github.com>
2025-01-17 15:11:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 app.get("/debug-sentry", function mainHandler(req, res) {
-  // throw new Error("My first Sentry error!");
+   throw new Error("My first Sentry error!");
 });
 
 app.post('/webhooks',clerkWebhooks)
