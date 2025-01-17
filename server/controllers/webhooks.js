1140fd4df3351abd44b21070396ef6c39afd220d
Manavp2305 <145106413+Manavp2305@users.noreply.github.com>
2025-01-17 15:31:39 +0530

    Update webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -14,21 +14,20 @@ export const clerkWebhooks = async (req, res) => {
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
+      "svix-signature": req.headers["svix-signature"]
     });
 
     //getting data from request body
 
-    const { data, type } = req.body;
+    const { data, type } = req.body
 
     //switch case for different event
 
     switch (type) {
       case 'user.created':{
-
         const userData = {
           _id:data.id,
-          email:data.email_address[0].email_address,
+          email:data.email_addresses[0].email_address,
           name:data.first_name+" "+data.last_name,
           image:data.image_url,
           resume:'' 
@@ -36,24 +35,21 @@ export const clerkWebhooks = async (req, res) => {
         await User.create(userData)
         res.json({})
         break;
-
       }
 
       case 'user.updated':{
-        
         const userData = {
-          email:data.email_address[0].email_address,
+          email:data.email_addresses[0].email_address,
           name:data.first_name+" "+data.last_name,
           image:data.image_url,
         }
-        await User.FindByIdAndUpdate(data.id,userData)
+        await User.findByIdAndUpdate(data.id,userData)
         res.json({})
         break;
-
       }
 
       case 'user.deleted':{
-        await User.FindByIdAndDelete(data.id)
+        await User.findByIdAndDelete(data.id)
         res.json({})
         break;
       }
