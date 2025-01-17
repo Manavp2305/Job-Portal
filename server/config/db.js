cfc5cfdb9e6f8a542e1c6c7b9b4ee5a7ae5b7299
Manav patel <manavp306@gmail.com>
2025-01-17 11:46:46 +0530

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+//  Function to connect to the database
+
+const connectDB = async () =>{
+  mongoose.connection.on('connected',()=> console.log('datbase connected'))
+
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+}
+
+export default connectDB
---
+++ server/controllers/webhooks.js
@@ -0,0 +1,69 @@
+import { Webhook } from "svix";
+import User from "../models/user.js";
+import { json } from "express";
+
+// Api controller to manage clerk user with database
+
+export const clerkWebhooks = async (req, res) => {
+  try {
+    // Create svix instance with clerk webhook secrets
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // verifying headers
+
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    //getting data from request body
+
+    const { data, type } = req.body;
+
+    //switch case for different event
+
+    switch (key) {
+      case 'user.created':{
+
+        const userData = {
+          _id:data.id,
+          email:data.email_address[0].email_address,
+          name:data.first_name+" "+data.last_name,
+          image:data.image_url,
+          resume:'' 
+        }
+        await User.create(userData)
+        res.json({})
+        break;
+
+      }
+
+      case 'user.updated':{
+        
+        const userData = {
+          email:data.email_address[0].email_address,
+          name:data.first_name+" "+data.last_name,
+          image:data.image_url,
+        }
+        await User.FindByIdAndUpdate(data.id,userData)
+        res.json({})
+        break;
+
+      }
+
+      case 'user.deleted':{
+        await User.FindByIdAndDelete(data.id)
+        res.json({})
+        break;
+      }
+
+      default:
+        break;
+    }
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({sucess:false, message:'webhooks Error'});
+  }
+};
